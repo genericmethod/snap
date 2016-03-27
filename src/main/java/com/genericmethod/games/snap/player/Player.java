4dccaabc7c1a772d40d0cd86a157f9686ec1a935
cfarrugia <chris@yobetit.com>
2016-03-27 17:21:33 +0200

    Adds the Player Class and basic test

---
+++ src/main/java/com/genericmethod/games/snap/player/Player.java
@@ -0,0 +1,43 @@
+package com.genericmethod.games.snap.player;
+
+import com.genericmethod.games.snap.card.Card;
+
+import java.util.Stack;
+
+/**
+ * Represents a player in a game of cards
+ */
+public class Player {
+
+    private Stack<Card> cards;
+    private final String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+        cards = new Stack<Card>();
+    }
+
+    /**
+     * Give the player a card
+     */
+    public void addCard(Card card){
+        cards.push(card);
+    }
+
+    /**
+     * Player plays a card.
+     * This removes a card from the top of the stack
+     * @return the card that the player has played
+     */
+    public Card playCard(){
+        return cards.pop();
+    }
+
+    public Stack<Card> getCards() {
+        return cards;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+}
---
+++ src/test/java/com/genericmethod/games/snap/player/PlayerTest.java
@@ -0,0 +1,34 @@
+package com.genericmethod.games.snap.player;
+
+import com.genericmethod.games.snap.card.Card;
+import com.genericmethod.games.snap.enums.Rank;
+import com.genericmethod.games.snap.enums.Suit;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+
+    @Test
+    public void testPlayerInit(){
+        Player player = new Player("Matt");
+        assertEquals(0, player.getCards().size());
+    }
+
+    @Test
+    public void testAddCard(){
+        Player player = new Player("Matt");
+        player.addCard(new Card(Rank.ACE, Suit.CLUBS));
+        assertEquals(1, player.getCards().size());
+    }
+
+    @Test
+    public void testPlayCard(){
+        Player player = new Player("Matt");
+        player.addCard(new Card(Rank.ACE, Suit.CLUBS));
+        assertEquals(1, player.getCards().size());
+        player.playCard();
+        assertEquals(0, player.getCards().size());
+    }
+
+}
